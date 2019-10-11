93246e527a09772fa40db123606dc1840cb41143
Jorge <junior-soares42@hotmail.com>
2019-10-11 11:25:26 -0300

    adicionando

--- caxeiroviajante.py
+++ caxeiroviajante.py
@@ -10,57 +10,59 @@ def cidades():
 	return allcity
 
 
-def random_cromossomo():
-	listafin = []
+def random_cromossomo(allcity):
 	listaindi = []
-	prim = cidades()[randint(0,4)]
+	cont = 0
+	prim = allcity[randint(0,4)]
 	listaindi.append(prim)
-	sec = cidades()[randint(0,4)]
-	ter = cidades()[randint(0,4)]
-	quar = cidades()[randint(0,4)]
-	quin = cidades()[randint(0,4)]
+	sec = allcity[randint(0,4)]
+	ter = allcity[randint(0,4)]
+	quar = allcity[randint(0,4)]
+	quin = allcity[randint(0,4)]
 	while sec in listaindi:
-		sec = cidades()[randint(0,4)]
+		sec = allcity[randint(0,4)]
 	listaindi.append(sec)
 	while ter in listaindi:
-		ter = cidades()[randint(0,4)]
+		ter = allcity[randint(0,4)]
 	listaindi.append(ter)
 	while quar in listaindi:
-		quar = cidades()[randint(0,4)]
+		quar = allcity[randint(0,4)]
 	listaindi.append(quar)
 	while quin in listaindi:
-		quin = cidades()[randint(0,4)]
+		quin = allcity[randint(0,4)]
 	listaindi.append(quin)
+	return listaindi
 
-	if listaindi is not listafin:
-		listafin.append(listaindi)
-	return listafin
-
-#def random_populacao:
-
+def random_populacao():
+	cont = 0
+	lista = []
+	while cont<30:
+		a = random_cromossomo(cidades())
+		while a in lista:
+			a = random_cromossomo(cidades())
+		lista.append(a)
+		cont+=1
+	return lista
 
 def distancia2pt(x1,x2,y1,y2):
 	dbp = (((x2-x1)**2) + ((y2-y1)**2))**(1/2)
 	return dbp
 
 def funcapt(lista):
-	lista2 = []
+	lista_distancias = []
 	for i in range(len(lista)):
 		aux = 0
-		for j in range(len(lista[i])-1):
-			var = distancia2pt(lista[i][j][0],lista[i][j+1][0],lista[i][j][1],lista[i][j+1][1])
-			aux = aux + var
-		lista2.append("{:.2f}".format(aux))
-	return lista2
-
+		for j in range(len(lista[i]) - 1):
+			distancia2pt = (((lista[i][j][1]- lista[i][j][0])**(2)) + ((lista[i][j+1][1]- lista[i][j+1][0])**(2))) **(0.5)
+			aux  = aux + distancia2pt
+		lista_distancias.append(float("{:.2f}".format(aux)))
+	return lista_distancias
 def valorviagem(lista):
-	#para cada metro ele gasta 0.50 centavos
 	listavalor = []
 	for i in range(len(lista)):
 		listavalor.append(float(lista[i])*0.5)
 
 	return listavalor
 
-a = valorviagem(funcapt(random_cromossomo()))
-for i in range(len(a)):
-	print(a[i])
+print(random_populacao())
+print(funcapt(random_populacao()))
