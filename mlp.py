549c442ad0c3d34f4ad2899ceab47ed63d0cf7ed
Jorge <junior-soares42@hotmail.com>
2019-10-15 10:58:00 -0300

    usando regressor no mlp

--- mlp.py
+++ mlp.py
@@ -1,8 +1,34 @@
 from sklearn.neural_network import MLPClassifier
-#X = [[0., 0.], [1., 1.]]
-X = [[35.76,35.5,35.22,34.95,34.62,34.28,33.93,33.57,33.22,32.89,32.58,32.25,31.93,31.65,31.4,31.15],[35.5,35.22,34.95,34.62,34.28,33.93,33.57,33.22,32.89,32.58,32.25,31.93,31.65,31.4,31.15,30.91]]
-y = [31, 30]
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(5, 2), random_state=1)
+from sklearn.neural_network import MLPRegressor
+import csv
+lista = []
+
+arquivo = open('valorsin.csv')
+linhas = csv.reader(arquivo)
+lista = []
+for linha in linhas:
+	l = []
+	for i in range(len(linha)):
+		l.append(float(linha[i]))
+	lista.append(l)
+X = lista
+
+arquivo = open('valorout.csv')
+linhas = csv.reader(arquivo) 
+lista = []
+for linha in linhas:
+	for i in range(len(linha)):
+		lista.append(float(linha[i]))
+
+y= lista
+
+print(X)
+print(y)
+clf = MLPRegressor(hidden_layer_sizes=(100, ), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', 
+	learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, 
+	verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, 
+	beta_1=0.9, beta_2=0.999, epsilon=1e-08, n_iter_no_change=10)
+#clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+#                    hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(X, y)
-print(clf.predict([[35.22,34.95,34.62,34.28,33.93,33.57,33.22,32.89,32.58,32.25,31.93,31.65,31.4,31.15,30.91,30.67]]))
+print(clf.predict([[59.99,60.00,76.3,83.4,78.35,64.36,70.6,59.99,70.04,89.8,113.2,81.8,92.2,72.38,85.9,92.2,88.7,96.9]]))
